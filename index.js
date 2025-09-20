6e0b04945c65ed0b2a60c7448349f27e770ced34
daniel fernando <daniel.fernando@indomobil.co.id>
2025-09-20 21:38:14 +0700

    penambahan end point /chat dan Front end di folder public

--- index.js
+++ index.js
@@ -15,13 +15,15 @@ const geminiModels = {
     text:"gemini-2.5-flash-lite",
     image:"gemini-2.5-flash",
     audio:"gemini-2.5-flash",
-    document:"gemini-2.5-flash-lite"
+    document:"gemini-2.5-flash-lite",
+    chat: 'gemini-2.5-flash-lite'
 };
 const extractGeneratedText = (data) => {
 }
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static('public'));
 
 app.post('/generate-text', async (req, res) => {
     // handle bagaimana request diterima oleh user
@@ -81,6 +83,70 @@ app.post('/generate-text-from-image', upload.single('image'), async (req, res) =
     } catch (err) {
         res.status(500).json({ message: err.message });
     }
+});
+
+app.post('/chat', async (req, res) => {
+  const { conversation } = req.body;
+  // const conversation = req.body.conversation;
+
+  // Guard clause 1 -- cek conversation-nya itu array atau bukan
+  if (!conversation || !Array.isArray(conversation)) {
+    res.status(400).json({
+      success: false,
+      data: null,
+      message: 'Percakapan tidak valid!'
+    });
+  }
+
+  // Guard clause 2 -- cek integritas data-nya
+  let dataIsInvalid = false; // semantic
+
+  conversation.forEach(item => {
+    if (!item) {
+      dataIsInvalid = true;
+    } else if (typeof item !== 'object') {
+      dataIsInvalid = true;
+    } else if (!item.role || !item.message) {
+      dataIsInvalid = true;
+    }
+  });
+
+  if (dataIsInvalid) {
+    return res.status(400).json({
+      success: false,
+      data: null,
+      message: 'Ada data yang invalid pada percakapan yang dikirim.'
+    });
+  }
+
+  // Mapping
+  const contents = conversation.map(item => {
+    return {
+      role: item.role,
+      parts: [
+        { text: item.message }
+      ]
+    }
+  });
+
+  try {
+    const aiResponse = await ai.models.generateContent({
+      model: geminiModels.chat,
+      contents // object property shorthand
+    });
+
+    return res.status(200).json({
+      success: true,
+      data: aiResponse.text,
+      message: null
+    });
+  } catch (e) {
+    return res.status(e.code || 500).json({
+      success: false,
+      data: null,
+      message: e.message
+    });
+  }
 });
 
 app.listen(port, () => {
